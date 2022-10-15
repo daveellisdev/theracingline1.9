f4f40ff4796396d3256ed8c5c6fa827558292dd8
Dave Ellis <dave@daveellis.dev>
2022-10-15 15:40:04 +0100

    Initial commit

---
+++ theracinglineApp.swift
@@ -0,0 +1,17 @@
+//
+//  theracinglineApp.swift
+//  theracingline
+//
+//  Created by Dave on 15/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct theracinglineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
