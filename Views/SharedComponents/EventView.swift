863ccb42cd72a445946423e1e65d864a114db135
Dave Ellis <dave@daveellis.dev>
2023-05-04 18:43:03 +0100

    Hide banner upon subscription
    Hidden

--- Views/SharedComponents/EventView.swift
+++ Views/SharedComponents/EventView.swift
@@ -25,15 +25,15 @@ struct EventView: View {
         
         ScrollView {
             VStack {
-                
-                Button {
-                    showingFilterSheet = true
-                } label: {
-                    PremiumBarSlim()
-                }.sheet(isPresented: $showingFilterSheet){
-                    SubscriptionView(dc: dc, sm: sm)
+                if !sm.subscribed {
+                    Button {
+                        showingFilterSheet = true
+                    } label: {
+                        PremiumBarSlim()
+                    }.sheet(isPresented: $showingFilterSheet){
+                        SubscriptionView(dc: dc, sm: sm)
+                    }
                 }
-
                 GroupBox {
                     VStack(alignment: .leading) {
                         EventRowSeriesList(dc: dc, raceEvent: event)
